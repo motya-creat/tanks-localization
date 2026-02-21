9199e7d9b59224799aaf6407b88fb784e671ee05
Your Name <you@example.com>
2026-02-21 15:36:42 +0300

    deploy

--- src/Tanks.jsx
+++ src/Tanks.jsx
@@ -1,24 +1,11 @@
 import { useTranslation } from "react-i18next"
+import tanks from './data.js'
+
 
 const Tank = () => {
 
     const { t } = useTranslation("about")
-
-    const baseUrlicons = '../src/images/icons/' 
-
-    const tanks = [
-        { id: 'tankOne', nation: `${baseUrlicons}germany.png`, imgUrl: 'https://i.ytimg.com/vi/ek0d_BmZuUo/maxresdefault.jpg',},
-        { id: "tankTwo", nation: `${baseUrlicons}germany.png`, imgUrl: 'https://yandex-images.clstorage.net/9NAHO6391/bcdc79SEJ/AM4b7YH7V4tYEOh0ePRwq3yih8TTZyL7fKajRrpkPqflPWM-pnpu3kK-PtnKUfYzO242b10pdvA-kLzXAgkpcEXHWuzk0kOHSDShUAi9fOtIl4v0ii4a6kHui0aabMcKAPS2pgv3ryerSk7IrkSzHgpvK_e4OIM0kiXI7-6kJAW8Ll8JkS_6p9w4PZQY1JALOHfXTBdukgqw0pY_07zv5qzQuv7rW7MPk3oe0IIMK1d-59tPiXiz7HqZeLvGLIihGL5biRG7Uv_0yPWkBBhwDyCfbzBn5n5-hNp2Wx-4d0Yk1OKKi3-no29ONmAOEJu3LgNHn_GsqwA-TVA3wpRYYaQuI_XdHzqHfCChyADQ1DegE69kh2b6yiwiBtuPYD_OHEwOenv7o1sHVo4cRoyPT74zZxvBjPOAmk0gi-YYODFAPlf57a_O67AoTejcXHTrDNsD9It2klJYnp5b4zC3_pR45oaPa0Mrv8oOWK5YZyuqL6dDJSR7PIJ5eIeicPxtGKajEVFzhleErC1oAOwMr1BTq0zncgLKXDbiZ9s8915EVKay8_sbE4fa_pxemANPentfP60IjxQC_fwPFiT8ZZQug62NixILPJwRFPg4cONEF3-U5z6O_uimZn-XPA_eAKQyEp9zq48zIp5sJnSDexLrs9eF7OMsPtnkt5o8oP3IRkfxkSved-RAOQRQ-Ahz2GcXzG-yTmq46g7_GwyLMlDsDk4_J6uXC8qGDIL0b0MG-3uXYfwPkHbR3E_GKDANFBIz_WULgpP4iLmkEGiElyjfM6DLJiY2kOpue_s4i044kLYGm4tr2y_2TlhabLfbmscvo2V8t5T-GVQz6liwhZxSu4WVG04vJBy1EJAkRPMwFzeUe4p2BmiyOq8XvF_uuKTWei-vQ5cXQmJoBhwXHzozY9NVXJfolhWIj84UeK1cyqfxjWOmd3zQhdDEfHgjvEsb9Cc-ypqsBu63y4iM' },
-        { id: "tankThree", nation: `${baseUrlicons}russia.png`, imgUrl: 'https://i.ytimg.com/vi/ETzbB_ecvts/maxresdefault.jpg' },
-        { id: "tankFour", nation: `${baseUrlicons}russia.png`, imgUrl: 'https://avatars.mds.yandex.net/i?id=db01faa475e071a69798c03a7f09ed7f_l-8897302-images-thumbs&n=13' },
-        { id: "tankFive", nation: `${baseUrlicons}uk.png`, imgUrl: 'https://avatars.mds.yandex.net/i?id=3fd97faa0fc9f08da3100298fa233ec7_l-12999039-images-thumbs&n=13' },
-        { id: "tankSix", nation: `${baseUrlicons}uk.png`, imgUrl: 'https://staticfiles.warthunder.com/upload/image/!2025/12/event/winter_concert_marathon/renders/1280x720_06_boxer_crv_block2_41f624b1ca6ee6123ae9657f31ed7d2e.jpg' },
-        { id: "tankSeven",nation: `${baseUrlicons}italy.png`,  imgUrl: 'https://i.ytimg.com/vi/NRhJmi5ZM80/maxresdefault.jpg' },
-        { id: "tankEight",nation: `${baseUrlicons}sweden.png`,  imgUrl: 'https://i.ytimg.com/vi/P4yluxvRDDs/maxresdefault.jpg' },
-        { id: "tankNine", nation: `${baseUrlicons}germany.png`, imgUrl: 'https://avatars.mds.yandex.net/i?id=24e4fe5fcb6516ee6d3a489598f15331_l-5231073-images-thumbs&n=13' },
-        { id: "tankTen", nation: `${baseUrlicons}russia.png`, imgUrl: 'https://i.ytimg.com/vi/lZ9RyhUNEXI/maxresdefault.jpg' }
-    ]
-
+    console.log(tanks)
     return (
         <div className="gallery">
             {tanks.map((tank) => {
